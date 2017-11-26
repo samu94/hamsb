7c74844e2a6d982136523ae4b2e952dd9268fea4
samu94 <samuelmartinezt94@gmail.com>
2017-11-26 17:01:54 +0100

    now it must rocks :-)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ id: "It works :)" });
+  res.send({ id: "He hecho cambios :)" });
 });
 
 const PORT = process.env.PORT || 5000;
